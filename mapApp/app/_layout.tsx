055a803841cd52434a569e6d1cf186bc156c76aa
richardz403 <116985684+richardz403@users.noreply.github.com>
2025-04-08 17:17:42 -0700

    stuff

---
+++ mapApp/app/_layout.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import { Stack } from 'expo-router';
+
+const Layout = () => {
+    return (
+        <Stack>
+            <Stack.Screen name ="index" options={{title: 'Home'}} />
+        </Stack>
+    );
+}
+
+export default Layout;
